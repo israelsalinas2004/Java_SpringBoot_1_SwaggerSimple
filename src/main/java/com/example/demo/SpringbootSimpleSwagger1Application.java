7cf4658c483ed3b811c97deb9658a1d995e53271
israel salinas contreras <israel_salinas2004@hotmail.com>
2023-11-23 10:56:05 -0600

    My first

---
+++ src/main/java/com/example/demo/SpringbootSimpleSwagger1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimpleSwagger1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSimpleSwagger1Application.class, args);
+	}
+
+}
